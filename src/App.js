f2c92a8445c951476206dfba5367ed5b84bc0dc4
aaron.hindes <aaron.hindes@domo.com>
2021-05-29 18:23:56 -0600

    Delete image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Change
         </a>
       </header>
     </div>
